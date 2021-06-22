49034f22e4ab681a34fdba4dd9ba8c8af21e5285
ChanAndKlee <ariya.phe@student.mahidol.edu>
2021-06-22 23:00:49 +0700

    Exercise_Ariya_P.py
    Collect student score (basic)

---
+++ Exercise_Ariya_P.py
@@ -0,0 +1,14 @@
+# Input from user #
+print("-- Sophomore's Score Semester 1 --\nEnter your score respectively:")
+foundEng = input("Foundation English: ")
+genBusi = input("General Business: ")
+introCS = input("Introduction to Computer Systems: ")
+comProg = input("Computer Programming: ")
+
+
+# Output #
+print("\n--- Your Score ---")
+print("Foundation English: ", foundEng)
+print("General Business: ", genBusi)
+print("Introduction to Computer Systems: ", introCS)
+print("Computer Programming: ", comProg)
