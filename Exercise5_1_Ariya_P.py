1978338fabd28e37f709da2241b6d601fe64f73a
ChanAndKlee <ariya.phe@student.mahidol.edu>
2021-06-23 21:49:05 +0700

    Exercise 5 (1&2)
    Operates two variables, Physics

---
+++ Exercise5_1_Ariya_P.py
@@ -0,0 +1,21 @@
+# Operates two variables with operators
+
+x = int(input("x: "))
+y = int(input("y: "))
+print(x,"+",y,"=",x+y)
+print(x,"-",y,"=",x-y)
+print(x,"*",y,"=",x*y)
+print(x,"/",y,"=",x/y)
+
+
+"""
+!! Beware of ... !!
+- (+) can't use with two different data types like int and str
+ex: x = "1"
+    y = 5
+    z = x + y
+    print(z)        // Error => Runtime Error because you can't concatenate two variables that's not same type.
+                    // Can fix by => Change to same type (int int, str str)
+                                     ex: x = int("1")  or
+                                         y = str(5)
+"""
---
+++ Exercise5_2_Ariya_P.py
@@ -0,0 +1,7 @@
+# Physics Formula
+# v = s/t, find velocity
+
+s = float(input("s (distance): "))      # In case that it's not only integer
+t = float(input("t (time): "))
+v = s/t
+print("v (velocity):",v,"km/h")
