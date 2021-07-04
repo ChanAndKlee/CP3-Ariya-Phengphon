e5df7a14d2a1717081a5832abb593b7702e61d04
Ariya Phengphon <81862069+ChanAndKlee@users.noreply.github.com>
2021-07-05 00:56:15 +0700

    Note

--- hello.py
+++ hello.py
@@ -1,2 +1,33 @@
-name = input("Name:")
-print("Hello "+name)
+# Import Library
+# Click Ctrl + that command to view it
+"""
+import math
+print(math.ceil(10.59))     # ปัดขึ้น เสมอ
+print(math.floor(10.59))    # ปัดลง เสมอ
+print(math.factorial(5))    # 5! = 5*4*3*2*1
+print(math.pow(4,2))        # 4^2 = 16
+"""
+
+# Manage String
+"""
+text = "Hello"
+print(text[0:4])            # o - before 4
+print(text[1:])             # ตัวที่ 1 ยันตัวสุดท้าย
+address = "642/17 Sukhumvit Bangkok"
+print("Bangkok" in address)  # True
+"""
+
+# String format, same as C Language
+"""
+name = "Chan"
+surname = "PP"
+print("Hello %s %s" % (name,surname))
+
+name = "chan"
+print("hello "+name.capitalize())       # .capitalize() convert first char to uppercase, and other small
+textFormatted = "Welcome"
+print(textFormatted.center(20,"="))     # User 20 places for all (text included)
+
+print(len(name))                        # Count characters
+"""
+
