fd7183b4dfd80dbfe855d8f3e632c6c8f3fba481
Ariya Phengphon <81862069+ChanAndKlee@users.noreply.github.com>
2021-07-08 17:13:00 +0700

    Update Lecture73_Ariya_P.py

--- Lecture73_Ariya_P.py
+++ Lecture73_Ariya_P.py
@@ -1,24 +1,31 @@
-# List in List
+# Input Menu, calculate price automatically
+systemMenu = {'เนื้อปิ้ง':15,'หมูปิ้ง':10,'ไก่ปิ้ง':10,'ข้าวเหนียว':5}  # All available Menu
+menuList = []    # Customer's Menu List
 
-menuList = []
 def showSlipt():
     print("--- My Food ---")
     totalPrice = 0
-    var = menuList[][]
-    print(var)
     for i in range(len(menuList)):
-        print(menuList[i][0],":",menuList[i][1])    # Normally menuList[i] refers to one big block in list like [1,2]
-        totalPrice += int(menuList[i][1])           # menuList[i][0] -> refers to 'first word' in one block
+        print(menuList[i][0],menuList[i][1])
+        totalPrice += menuList[i][1]
     return totalPrice
 
+def printMenu():
+    print("==> Menu here <==")
+    print("1. เนื้อปิ้ง     15 Baht")
+    print("2. หมูปิ้ง     10 Baht")
+    print("3. ไก่ปิ้ง      10 Baht")
+    print("4. ข้าวเหนียว   5 Baht")
+
 # Main Function
+printMenu()
 while True:
-    menu = input("Enter menu : ")
+    menu = input("Enter menu : ")   # Input Menu from customer
     if menu.lower() == "exit":
         break
     else:
-        price = input("Price : ")
-        menuList.append([menu,price])       # Add List in List   [[1,2],[3,4]]
-print(menuList)
+        menuList.append([menu,systemMenu[menu]])         # Add List in List   [[1,2],[3,4]]
+# print(systemMenu.get('ข้าวเหนียว'))                       # Get Value by using key
+# print(menuList)
 print("Total Price :",showSlipt(),"Baht")
 print("Thank you!")
